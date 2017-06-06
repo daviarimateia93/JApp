9ab9a5713f912ec8abc36d139189be3907d3f7ce
Davi Arimateia <daviarimateia93@gmail.com>
2017-06-05 21:34:22 -0300

    boolean helper

---
+++ JAppUtil/src/main/java/japp/util/BooleanHelper.java
@@ -0,0 +1,16 @@
+package japp.util;
+
+public abstract class BooleanHelper {
+	
+	protected BooleanHelper() {
+		
+	}
+	
+	public static Boolean safeValueOf(final String string) {
+		if (StringHelper.isNullOrBlank(string)) {
+			return null;
+		} else {
+			return Boolean.valueOf(string);
+		}
+	}
+}
