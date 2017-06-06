9991824519e525fab981d8fca8a808773227907e
Davi Arimateia <daviarimateia93@gmail.com>
2017-06-05 21:48:00 -0300

    refactoring deep search

--- JAppModel/src/main/java/japp/model/jpa/repository/Repository.java
+++ JAppModel/src/main/java/japp/model/jpa/repository/Repository.java
@@ -1,9 +1,13 @@
 package japp.model.jpa.repository;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.ThreadLocalRandom;
 
 import javax.persistence.EntityManager;
@@ -20,6 +24,7 @@ import javax.persistence.criteria.Selection;
 import japp.model.entity.Entity;
 import japp.model.jpa.repository.search.PageResult;
 import japp.model.jpa.repository.search.SelectionWrapper;
+import japp.util.DateHelper;
 import japp.util.JAppRuntimeException;
 import japp.util.Setable;
 
@@ -345,6 +350,72 @@ public abstract class Repository<T extends Entity, U> {
 		return new PageResult<>(convertToMappedResultList(aliases, resultList), total, totalFiltered, firstResult, (long) maxResults);
 	}
 	
+	public PageResult<Map<String, Object>> deepSearch(final List<String> selections, final String fromQueryString, final String criteriaQuery, final String sortQueryString, final int firstResult, final int maxResults, final Object... parameters) {
+		return deepSearch(selections, fromQueryString, parseQuery(criteriaQuery), sortQueryString, firstResult, maxResults, parameters);
+	}
+	
+	public PageResult<Map<String, Object>> deepSearch(final List<String> selections, final String fromQueryString, final List<String> criteriaQuery, final String sortQueryString, final int firstResult, final int maxResults, final Object... parameters) {
+		final List<String> newCriteriaQuery = new ArrayList<>();
+		final List<Object> newParameters = new ArrayList<>();
+		final List<Object> newStringParameters = new ArrayList<>();
+		final Map<Integer, String> replaceStringParameters = new HashMap<>();
+		final Setable<Integer> criteriaQueryFragmentStart = new Setable<>(0);
+		
+		for (int i = 0; i < parameters.length; i++) {
+			final Object parameter = parameters[i];
+			
+			newParameters.add(parameter);
+			
+			if (parameter != null && parameter instanceof String) {
+				final String parameterAsString = (String) parameter;
+				final String[] parameterFragments = parameterAsString.split(" +");
+				final List<String> duplicatedCriteriaQueryFragment = new ArrayList<>();
+				final List<String> criteriaQueryFragmentComplement = new ArrayList<>();
+				final String criteriaQueryFragment = getQueryFragment(criteriaQuery, criteriaQueryFragmentStart, i, criteriaQueryFragmentComplement);
+				
+				duplicatedCriteriaQueryFragment.add("(");
+				duplicatedCriteriaQueryFragment.add(criteriaQueryFragment);
+				
+				newCriteriaQuery.addAll(criteriaQueryFragmentComplement);
+				
+				for (int j = 1; j < parameterFragments.length; j++) {
+					duplicatedCriteriaQueryFragment.add(" OR ");
+					duplicatedCriteriaQueryFragment.add(criteriaQueryFragment.replace("?" + i, "?" + (parameters.length + newStringParameters.size() + (j - 1))));
+				}
+				
+				duplicatedCriteriaQueryFragment.add(")");
+				
+				Collections.addAll(newCriteriaQuery, duplicatedCriteriaQueryFragment.toArray(new String[duplicatedCriteriaQueryFragment.size()]));
+				
+				if (parameterFragments.length > 1) {
+					replaceStringParameters.put(i, fixParameterFragment(parameterFragments, parameterFragments[0]));
+					
+					for (int j = 1; j < parameterFragments.length; j++) {
+						newStringParameters.add(fixParameterFragment(parameterFragments, parameterFragments[j]));
+					}
+				}
+			} else {
+				final List<String> criteriaQueryFragmentComplement = new ArrayList<>();
+				final String criteriaQueryFragment = getQueryFragment(criteriaQuery, criteriaQueryFragmentStart, i, criteriaQueryFragmentComplement);
+				
+				newCriteriaQuery.addAll(criteriaQueryFragmentComplement);
+				newCriteriaQuery.add(criteriaQueryFragment);
+			}
+		}
+		
+		for (int i = criteriaQueryFragmentStart.getValue(); i < criteriaQuery.size(); i++) {
+			newCriteriaQuery.add(criteriaQuery.get(i));
+		}
+		
+		newParameters.addAll(newStringParameters);
+		
+		for (final Map.Entry<Integer, String> entry : replaceStringParameters.entrySet()) {
+			newParameters.set(entry.getKey(), entry.getValue());
+		}
+		
+		return search(selections, fromQueryString, String.join(" ", newCriteriaQuery), sortQueryString, firstResult, maxResults, newParameters.toArray());
+	}
+	
 	protected List<Map<String, Object>> convertToMappedResultList(final List<String> aliases, final List<Object[]> resultList) {
 		final List<Map<String, Object>> mappedResultList = new ArrayList<>();
 		
@@ -414,5 +485,184 @@ public abstract class Repository<T extends Entity, U> {
 		}
 		
 		return map;
+	}
+	
+	protected Date parseStartDate(final String query) {
+		return getSmallestDate(parseDates(query, " 00:00:00", "yyyy-MM-dd HH:mm:ss"));
+	}
+	
+	protected Date parseEndDate(final String query) {
+		return getBiggestDate(parseDates(query, " 23:59:59", "yyyy-MM-dd HH:mm:ss"));
+	}
+	
+	protected Date parseStartDateTime(final String query) {
+		return getSmallestDate(parseDates(query, ":00", "yyyy-MM-dd HH:mm:ss"));
+	}
+	
+	protected Date parseEndDateTime(final String query) {
+		return getBiggestDate(parseDates(query, ":59", "yyyy-MM-dd HH:mm:ss"));
+	}
+	
+	protected Date parseDateTime(final String query) {
+		return DateHelper.parseDate(query, "yyyy-MM-dd HH:mm:ss");
+	}
+	
+	protected Date parseDate(final String query) {
+		return DateHelper.parseDate(query, "yyyy-MM-dd");
+	}
+	
+	private Date getSmallestDate(final Date[] dates) {
+		Date smallestDate = null;
+		
+		for (final Date date : dates) {
+			if (smallestDate == null || date.compareTo(smallestDate) <= 0) {
+				smallestDate = date;
+			}
+		}
+		
+		return smallestDate;
+	}
+	
+	private Date getBiggestDate(final Date[] dates) {
+		Date biggestDate = null;
+		
+		for (final Date date : dates) {
+			if (biggestDate == null || date.compareTo(biggestDate) >= 0) {
+				biggestDate = date;
+			}
+		}
+		
+		return biggestDate;
+	}
+	
+	private Date[] parseDates(final String query, final String complement, final String pattern) {
+		final Set<Date> dates = new HashSet<>();
+		final String[] queryDateFragments = getQueryDateFragments(query);
+		
+		for (final String queryDateFragment : queryDateFragments) {
+			final Date date = parseDate(queryDateFragment, complement, pattern);
+			
+			if (date != null) {
+				dates.add(date);
+			}
+		}
+		
+		return dates.toArray(new Date[dates.size()]);
+	}
+	
+	private Date parseDate(final String queryDateFragment, final String complement, final String pattern) {
+		return DateHelper.parseDate(queryDateFragment + complement, pattern);
+	}
+	
+	protected String getQueryGlue(final String query) {
+		final String[] splittedQuery = splitQuery(query);
+		int counter = splittedQuery.length;
+		
+		for (final String queryFragment : splittedQuery) {
+			final Date dateTime = parseDateTime(queryFragment);
+			final Date date = parseDate(queryFragment);
+			
+			if (dateTime != null || date != null) {
+				counter--;
+			}
+		}
+		
+		return counter > 0 && counter < splittedQuery.length ? "AND" : "OR";
+	}
+	
+	private String[] getQueryDateFragments(final String query) {
+		final Set<String> queryDateFragments = new HashSet<>();
+		final String[] splittedQuery = splitQuery(query);
+		
+		for (final String queryFragment : splittedQuery) {
+			final Date dateTime = parseDateTime(queryFragment);
+			final Date date = parseDate(queryFragment);
+			
+			if (dateTime != null || date != null) {
+				queryDateFragments.add(queryFragment);
+			}
+		}
+		
+		return queryDateFragments.toArray(new String[queryDateFragments.size()]);
+	}
+	
+	private String[] splitQuery(final String query) {
+		return query.trim().split(" +");
+	}
+	
+	private String fixParameterFragment(final String[] parameterFragments, final String parameterFragment) {
+		final String firstParameterFragment = parameterFragments[0];
+		final String lastParameterFragment = parameterFragments[parameterFragments.length - 1];
+		final StringBuilder stringBuilder = new StringBuilder();
+		
+		if (firstParameterFragment.startsWith("%") && !parameterFragment.startsWith("%")) {
+			stringBuilder.append('%');
+		}
+		
+		stringBuilder.append(parameterFragment);
+		
+		if (lastParameterFragment.endsWith("%") && !parameterFragment.endsWith("%")) {
+			stringBuilder.append('%');
+		}
+		
+		return stringBuilder.toString();
+	}
+	
+	private String getQueryFragment(final List<String> query, final Setable<Integer> start, final int index, final List<String> complement) {
+		for (int i = start.getValue(); i < query.size(); start.setValue(++i)) {
+			final String queryFragment = query.get(i);
+			
+			if (queryFragment.contains("?" + index)) {
+				start.setValue(++i);
+				return queryFragment;
+			} else {
+				complement.add(queryFragment);
+			}
+		}
+		
+		return "";
+	}
+	
+	private List<String> parseQuery(final String query) {
+		final List<String> queryFragments = new ArrayList<>();
+		final StringBuilder stringBuilder = new StringBuilder();
+		
+		boolean endStatement = true;
+		boolean endString = true;
+		
+		for (int i = 0; i < query.length(); i++) {
+			char previousCharacter = i > 0 ? query.charAt(i - 1) : '\u0000';
+			char character = query.charAt(i);
+			boolean skip = false;
+			
+			if (endString && character == '[') {
+				endStatement = false;
+				skip = true;
+				addQueryFragment(queryFragments, stringBuilder);
+			} else if (!endStatement && endString && character == ']') {
+				endStatement = true;
+				skip = true;
+				addQueryFragment(queryFragments, stringBuilder);
+			} else if (!endStatement && endString && character == '\'') {
+				endString = false;
+			} else if (!endStatement && !endString && character == '\'' && previousCharacter != '\\') {
+				endString = true;
+			}
+			
+			if (!skip) {
+				stringBuilder.append(character);
+			}
+		}
+		
+		addQueryFragment(queryFragments, stringBuilder);
+		
+		return queryFragments;
+	}
+	
+	private void addQueryFragment(final List<String> queryFragments, final StringBuilder stringBuilder) {
+		if (stringBuilder.length() > 0) {
+			queryFragments.add(stringBuilder.toString());
+			stringBuilder.setLength(0);
+		}
 	}
 }
