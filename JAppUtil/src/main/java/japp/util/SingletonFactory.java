e8340808e2f0b4e4d35138b854a48640ab17a76b
Davi Arimateia <daviarimateia93@gmail.com>
2017-07-05 22:22:33 -0300

    singleton instance per thread

--- JAppUtil/src/main/java/japp/util/SingletonFactory.java
+++ JAppUtil/src/main/java/japp/util/SingletonFactory.java
@@ -3,10 +3,12 @@ package japp.util;
 import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.Callable;
 
 public class SingletonFactory {
 	
 	private static final Map<Class<? extends Singletonable>, Singletonable> instances = new HashMap<>();
+	private static final ThreadLocal<Map<Class<?>, Object>> instancesPerThread = new ThreadLocal<>();
 	
 	@SuppressWarnings("unchecked")
 	public static <T extends Singletonable> T getInstance(final Class<T> instanceClass, final Class<?>[] parameterClasses, final Object... parameters) {
@@ -25,6 +27,24 @@ public class SingletonFactory {
 		return getInstance(instanceClass, new Class<?>[] {}, new Object[] {});
 	}
 	
+	public static <T extends Singletonable> T getInstancePerThread(final Class<T> instanceClass, final Class<?>[] parameterClasses, final Object... parameters) {
+		return getInstancePerThread(instanceClass, new Callable<T>() {
+			@Override
+			public T call() throws Exception {
+				return ReflectionHelper.newInstance(instanceClass, parameterClasses, parameters);
+			}
+		});
+	}
+	
+	public static <T extends Singletonable> T getInstancePerThread(final Class<T> instanceClass) {
+		return getInstancePerThread(instanceClass, new Callable<T>() {
+			@Override
+			public T call() throws Exception {
+				return ReflectionHelper.newInstance(instanceClass);
+			}
+		});
+	}
+	
 	@SuppressWarnings("unchecked")
 	public static <T extends Singletonable & ProxyInterceptable> T getProxyInterceptableInstance(final Class<T> instanceClass, final Class<?>[] parameterClasses, final Object... parameters) {
 		if (instances.containsKey(instanceClass)) {
@@ -39,6 +59,24 @@ public class SingletonFactory {
 	}
 	
 	@SuppressWarnings("unchecked")
+	public static <T extends Singletonable & ProxyInterceptable> T getProxyInterceptableInstancePerThread(final Class<T> instanceClass, final Class<?>[] parameterClasses, final Object... parameters) {
+		if (instancesPerThread.get().containsKey(instanceClass)) {
+			return (T) instancesPerThread.get().get(instanceClass);
+		} else {
+			return getInstancePerThread(instanceClass, new Callable<T>() {
+				@Override
+				public T call() throws Exception {
+					return Proxy.intercept(instanceClass, parameterClasses, parameters);
+				}
+			});
+		}
+	}
+	
+	public static <T extends Singletonable & ProxyInterceptable> T getProxyInterceptableInstancePerThread(final Class<T> instanceClass) {
+		return getProxyInterceptableInstance(instanceClass, new Class<?>[] {}, new Object[] {});
+	}
+	
+	@SuppressWarnings("unchecked")
 	protected static <T extends Singletonable> T getInstance(final T instance) {
 		if (instances.containsKey(instance.getClass())) {
 			return (T) instances.get(instance.getClass());
@@ -46,6 +84,26 @@ public class SingletonFactory {
 			instances.put(instance.getClass(), instance);
 			
 			return instance;
+		}
+	}
+	
+	@SuppressWarnings("unchecked")
+	protected static <T extends Singletonable> T getInstancePerThread(final Class<T> instanceClass, final Callable<T> callable) {
+		try {
+			if (instancesPerThread.get() == null) {
+				instancesPerThread.set(new HashMap<>());
+			}
+			
+			if (instancesPerThread.get().containsKey(instanceClass)) {
+				return (T) instancesPerThread.get().get(instanceClass);
+			} else {
+				T instance = callable.call();
+				instancesPerThread.get().put(instanceClass, instance);
+				
+				return instance;
+			}
+		} catch (final Exception exception) {
+			throw new JAppRuntimeException(exception);
 		}
 	}
 }
